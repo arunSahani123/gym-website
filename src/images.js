79c026e0157eba696790cf31353cb9e1d3e6bd4e
Arun Sahani <arunsahani123@gmail.com>
2026-07-15 08:34:21 +0530

    Update gym website

---
+++ src/images.js
@@ -0,0 +1,13 @@
+import heroBg from '../hero_bg.jpg';
+import trainerCardio from '../trainer_cardio.jpg';
+import trainerStrength from '../trainer_strength.jpg';
+import trainerYoga from '../trainer_yoga.jpg';
+
+export const images = {
+  heroBg,
+  trainerCardio,
+  trainerStrength,
+  trainerYoga,
+};
+
+export default images;
